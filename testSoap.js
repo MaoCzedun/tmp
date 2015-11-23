a75847dc45b791860b753411b0358d1f4da6ca86
MaoCzedun <alexpetrov-95@mail.ru>
2015-11-24 00:58:15 +0200

    sms curies working

--- testSoap.js
+++ testSoap.js
@@ -4,31 +4,22 @@ var url='http://turbosms.in.ua/api/wsdl.html',
     soapCoockie = require('soap-cookie'),
     report = require('./errorReporting').report,
     clientPhoneAlex="+380934683919",
-    clientPhoneVictoria="+300985885246",
+    clientPhoneVictoria="+380985885246",
     authData={login:"biglapa",password:"power1990"},
     sender="BigLapa.com",
-    responseHeaders;
+    sseid;
 
 soap.createClient(url,function(error,client){
      report(error);
      client.Auth(authData,function(error,result){
         report(error);
-        // console.log(result);
-
-        // console.log(client.get);
-        responseHeaders = client.lastResponseHeaders;
+        console.log(result);
+        client.setSecurity(new soapCoockie(client.lastResponseHeaders));
+        client.SendSMS({sender:"BigLapa.com",destination:clientPhoneVictoria,text:"Пять минут полет нормальный.Прием, прием, как слыщно меня? Прием...."},function(error,result){
+           report(error);
+            console.log(result);
+            console.log(client.lastRequestHeaders);
+        });
       });
-});
 
-soap.createClient(url,function(error,client){
-     report(error);
-     client.setSecurity(new soapCoockie(responseHeaders));
-      console.log();
-      console.log();
-      console.log();
-      client.SendSMS({sender:"BigLapa.com",destination:clientPhoneAlex,text:"Пять минут полет нормальный"},function(error,result){
-         report(error);
-          console.log(result);
-          console.log(client.lastResponseHeaders);
-      });
 });
