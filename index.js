3aaf4411c6b0004a5c8e6fab5767a2940b9075e7
MaoCzedun <alexpetrov-95@mail.ru>
2015-12-09 17:29:14 +0200

    delete config

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    config=require('./config/config.js').config,
     router = require('./helpers/router').router,
     app = express(),
     port = process.env.PORT || 8080;
-// app.set('views','./views');
-// app.set('view engine','ejs');
-config(app);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('./public'))
 app.use('/api',router);
-app.listen(port);
-console.log(`app start on port ${port}`);
+var server = app.listen(port,function(){
+  var host = server.address().address,
+      port = server.address().port;
+      console.log(`app http://${host}:${port}`);
+});
