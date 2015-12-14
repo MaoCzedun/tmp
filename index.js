b8e5260d6dfab22dc2984d4529987bcdcea72553
MaoCzedun <alexpetrov-95@mail.ru>
2015-12-15 00:55:21 +0200

    asdf

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     router = require('./helpers/router').router,
+    intel = require('intel'),
     app = express(),
     port = process.env.PORT || 8080;
+intel.setLevel(intel.TRACE);
+intel.addHandler(new intel.handlers.File('./logs/file.log'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('./public'))
 app.use('/api',router);
